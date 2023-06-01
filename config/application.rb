a1c31dc6b75ff4af84ebe250f345c18ab5d8691f
osigie <kenosagie88@gmail.com>
2023-06-01 12:55:23 +0100

    change app name

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module FoodappServer
+module EMarket
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
